f837fc84b12a8533ac4a65d6710a5359721455f7
pytbyte <heretolearn1@gmail.com>
2024-01-17 05:48:58 +0300

    async generators Tasks 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,19 +1,14 @@
-#!/bin/bash/env python3
-"""_async_generator_
-"""
+#!/usr/bin/env python3
+'''Task 0's module.
+'''
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """
-    Asynchronous generator function that produces
-    random numbers with a 1-second delay.
-
-    Yields:
-        int: A random integer between 1 and 10.
-    """
+    '''Generates a sequence of 10 numbers.
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
