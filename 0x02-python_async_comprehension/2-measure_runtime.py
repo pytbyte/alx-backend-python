d590c555a1afe0aeaa48f96ddf011ea10e0526cb
pytbyte <heretolearn1@gmail.com>
2024-01-17 05:51:53 +0300

    async generators Tasks 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,32 +1,21 @@
+#!/usr/bin/env python3
+'''Task 2's module.
+'''
 import asyncio
-from typing import List
+import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """
-    Measure the total runtime of executing
-    async_comprehension four times in parallel.
+    '''Executes async_comprehension 4 times and measures the
+    total execution time.
+    '''
+    start_time = time.time()
 
-    Returns:
-        float: Total runtime in seconds.
-    """
-    launch_time = asyncio.get_event_loop().time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
 
-    # Execute async_comprehension four times in parallel
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    end_time = time.time()
 
-    end_time = asyncio.get_event_loop().time()
-    total_runtime = end_time - launch_time
-
-    return total_runtime
-
-if __name__ == "__main__":
-    total_runtime = asyncio.run(measure_runtime())
-    print(f"Total runtime: {total_runtime:.2f} seconds")
+    total_time = end_time - start_time
+    return total_time
